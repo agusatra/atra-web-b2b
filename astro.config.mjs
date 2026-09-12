6ed77d21a2b0df4b1ac52c01b701c265d2db5890
Agus Atra <agusatra@gmail.com>
2026-09-12 12:49:58 +0700

    Mengubah konten utama menjadi landing page portfolio Atra Digital

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  adapter: vercel()
-});
+export default defineConfig({});
